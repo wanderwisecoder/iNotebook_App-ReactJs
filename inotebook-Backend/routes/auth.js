a30e78540cee7c1947bb6e11dc226b9e8256a3ad
wanderwisecoder <thissurajneversets@gmail.com>
2023-07-19 14:46:32 +0530

    Hashing Passwords using bcryptjs in NodeJs and adding jwt token to verify user and prvent it from getting hacked

--- inotebook-Backend/routes/auth.js
+++ inotebook-Backend/routes/auth.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const JWT_SECRET = 'WanderWiseTraveller';
 
 //Create a User using: POST "/api/auth/createUser". No Login Required.
 
@@ -22,13 +25,27 @@ router.post('/createuser', [body('name', 'Enter a valid name.').isLength({ min: 
 		if (user) {
 			return res.status(400).json({ error: 'Sorry a user with this email already exists.' });
 		}
+		const salt = await bcrypt.genSalt(10);
+
+		const secPass = await bcrypt.hash(req.body.password, salt);
+
 		//creating a new user
 		user = await User.create({
 			name: req.body.name,
-			password: req.body.password,
+			password: secPass,
 			email: req.body.email,
 		});
-		res.json({ user });
+		const data = {
+			user: {
+				id: user.id,
+			},
+		};
+
+		const authtoken = jwt.sign(data, JWT_SECRET);
+		console.log(authtoken);
+
+		//res.json({ user });
+		res.json({ authtoken });
 	} catch (error) {
 		//catching the error.
 		console.log(error.message);

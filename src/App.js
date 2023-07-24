39cafca26e4b71f2cfcd1e60eaaef7f89e8d01be
wanderwisecoder <thissurajneversets@gmail.com>
2023-07-24 12:26:23 +0530

    useContext hook: Using React Context API

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
+import NoteState from './context/notes/NoteState';
 
 function App() {
 	return (
 		<>
-			<Router>
-				<Navbar />
-				<Routes>
-					<Route exact path='/' element={<Home />}></Route>
-					<Route exact path='/about' element={<About />}></Route>
-				</Routes>
-			</Router>
+			<NoteState>
+				<Router>
+					<Navbar />
+					<Routes>
+						<Route exact path='/' element={<Home />}></Route>
+						<Route exact path='/about' element={<About />}></Route>
+					</Routes>
+				</Router>
+			</NoteState>
 		</>
 	);
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
 		<nav className='navbar navbar-expand-lg bg-body-tertiary'>
 			<div className='container-fluid'>
 				<Link className='navbar-brand' to='/'>
-					Navbar
+					iNotebook
 				</Link>
 				<button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
 					<span className='navbar-toggler-icon'></span>

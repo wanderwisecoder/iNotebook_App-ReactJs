9ed6d8ae10e77a5b122e65b482161c24c4cb11ae
wanderwisecoder <thissurajneversets@gmail.com>
2023-07-24 11:18:47 +0530

    Created Navbar and React Router Setup

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+const Home = () => {
+	return <div>This is Home.</div>;
+};
+
+export default Home;
